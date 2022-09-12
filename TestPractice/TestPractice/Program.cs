377eefaea3e44919b3598cc227fbc3d3d142366d
Koen94er <Koen_Wang@163.com>
2022-09-12 23:18:46 +0800

    koen-test-update switch

--- TestPractice/TestPractice/Program.cs
+++ TestPractice/TestPractice/Program.cs
@@ -32,28 +32,59 @@ namespace RectangleApp
     //        r.AcceptDetail();
     //        r.Display();
     //        Console.ReadLine();
-        //}
-    public class Program123
+    //}
+    //public class Program123
+    //{
+    //    static void Main(string[] args)
+    //    {
+
+    //        /* 局部变量定义 */
+    //        int a = 100;
+
+    //        /* 检查布尔条件 */
+    //        if (a < 20)
+    //        {
+    //            /* 如果条件为真，则输出下面的语句 */
+    //            Console.WriteLine("a 小于 20");
+    //        }
+    //        else
+    //        {
+    //            /* 如果条件为假，则输出下面的语句 */
+    //            Console.WriteLine("a 大于 20");
+    //        }
+    //        Console.WriteLine("a 的值是 {0}", a);
+    //        Console.ReadLine();
+    //    }
+    //}
+    class Program
     {
         static void Main(string[] args)
         {
-
-            /* 局部变量定义 */
-            int a = 100;
-
-            /* 检查布尔条件 */
-            if (a < 20)
+            int day = 6;
+            switch (day)
             {
-                /* 如果条件为真，则输出下面的语句 */
-                Console.WriteLine("a 小于 20");
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
             }
-            else
-            {
-                /* 如果条件为假，则输出下面的语句 */
-                Console.WriteLine("a 大于 20");
-            }
-            Console.WriteLine("a 的值是 {0}", a);
-            Console.ReadLine();
         }
     }
 }
